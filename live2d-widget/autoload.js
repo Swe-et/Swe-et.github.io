528020ace4370be7d19a768ff80074a1eaf2a3ce
Swe-et <473328746@qq.com>
2026-09-15 06:00:09 +0800

    Site updated: 2026-09-15 05:59:50

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -27,13 +27,13 @@ function loadExternalResource(url, type) {
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
 	Promise.all([
-		loadExternalResource(live2d_path + "waifu.css", "css"),
+		loadExternalResource(live2d_path + "waifu.css?v=20260915-2", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
 		// 配置选项的具体用法见 README.md
 		initWidget({
-			waifuPath: live2d_path + "waifu-tips.json",
+			waifuPath: live2d_path + "waifu-tips.json?v=20260915-3",
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			cdnPath: "/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
